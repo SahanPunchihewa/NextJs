84d26b912d2d24596b513e14a03186837808255f
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-04-13 18:25:41 +0530

    Routes

---
+++ nextjs/pages/about.js
@@ -0,0 +1,16 @@
+const About =() =>{
+
+    return ( 
+
+        <div>
+            <h1>About</h1>
+            <p>Hi My name is Sahan Punchihewa</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita consectetur, mollitia sint non repudiandae asperiores commodi accusamus, earum omnis facilis perferendis architecto impedit. Earum illo ut minus, eligendi maiores id.</p>
+
+        </div>
+
+    );
+
+}
+
+export default About;
