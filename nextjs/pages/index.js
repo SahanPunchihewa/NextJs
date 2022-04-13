db6ebe29b64211e0bd5af5cc3a0efbf8bc8a33be
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-04-13 23:50:51 +0530

    Layout Component

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -7,16 +7,13 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+      
       <h1>Home page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ea eaque deserunt assumenda facilis doloremque placeat modi unde quos inventore. Reprehenderit molestiae obcaecati ea tempora est, sunt rerum amet recusandae!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptates aspernatur dolor magnam. Repellendus voluptate dicta nesciunt ut corporis, earum facere obcaecati illum autem quas distinctio consectetur pariatur laboriosam illo.</p>
       <Link href= "/ninjas">
         <a>See Ninja Listing</a>
       </Link>
-    
-
-    <Footer/>
     </div>
   )
 }
